e9b127bc17c381b9464c9bce682fd82ebe946e37
Vaisakh-RS <vaisakhrs4@gmail.com>
2023-08-29 12:43:52 +0530

    Create a grid of 16x16 squares

--- script.js
+++ script.js
@@ -9,15 +9,18 @@ function createRow()
 
     for(let i=0;i<numberOfRows;i++)
     {
-    
-    
+        for(let j=0;j<numberOfColumns;j++)
+        {
             let cell=document.createElement("div");
-            cell.style.cssText='width: 49px;height:49px;outline:0.01px black solid;background-color:white;display:inline-block;`'
+            cell.style.cssText='width: 49px;height:49px;outline:0.0001px black solid;background-color:white;display:inline-block;margin-top:-2px'
             container.appendChild(cell);
 
+        }
+    
+           
     }
 
 }
 
 createRow()
-createRow()
+
