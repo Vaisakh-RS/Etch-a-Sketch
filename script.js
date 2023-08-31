b5635dd4eeadc2467761d89f3251edf9ef0aa34b
Vaisakh-RS <vaisakhrs4@gmail.com>
2023-08-31 14:34:56 +0530

    Implement the click and hold functionality to make the drawing smooth
    Mousedown,mousup and mouseover event listeners where used to implement the same.Also a set timeout was used.

--- script.js
+++ script.js
@@ -4,29 +4,44 @@ let container=document.getElementsByClassName("container")[0];
 
 function createGrid()
 {
-    let numberOfRows=16;
-    let numberOfColumns=16;
+    let isMouseDown = false;
+    let timerId;
+    let numberOfRowsAndColumns=16;
 
-    for(let i=0;i<numberOfRows;i++)
+    for(let i=0;i<numberOfRowsAndColumns;i++)
     {
-        for(let j=0;j<numberOfColumns;j++)
+        for(let j=0;j<numberOfRowsAndColumns;j++)
         {
             let cell=document.createElement("div");
             cell.style.cssText='background-color:white;flex-basis:45px';
             container.appendChild(cell);
-            cell.addEventListener("click",changeColor);
-            cell.addEventListener("mouseDown",changeColor);
+            cell.addEventListener("mousedown",()=>{
+                isMouseDown=true;
 
-        }
-    
-           
+                timerId=setTimeout(()=>{
+                    if(isMouseDown){
+                        cell.style.backgroundColor="red";
+                    }
+                },0.01);
+            });
+
+            cell.addEventListener("mouseup",()=>{
+                isMouseDown=false;
+                clearTimeout(timerId);
+            });
+
+            cell.addEventListener("mouseover",()=>{
+                if(isMouseDown){
+                    cell.style.backgroundColor="red";
+                }});
+        }  
     }
 
 }
 
 function changeColor()
 {
-    this.style.backgroundColor="red";
+    
 }
 
 createGrid();
