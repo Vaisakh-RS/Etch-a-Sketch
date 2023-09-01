9f01c73a6abc84dfad9c994edb6d135c35e3bc38
Vaisakh-RS <vaisakhrs4@gmail.com>
2023-09-01 23:29:51 +0530

    Add reset functionality to clear the canvas

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function createGrid()
         for(let j=0;j<numberOfRowsAndColumns;j++)
         {
             let cell=document.createElement("div");
+            cell.classList.add("cell");
             //flex basis is the width of each cell here
             cell.style.cssText='background-color:white;flex-basis:35px';
             container.appendChild(cell);
@@ -39,6 +40,18 @@ function createGrid()
     }
 
 }
+
+
+
+let cells=document.getElementsByClassName("cell");
+let resetButton=document.getElementById("reset");
+resetButton.addEventListener("click",()=>{
+    for(i=0;i<256;i++)
+    {
+        cells[i].style.backgroundColor="white";
+    }
+       
+});
 
 createGrid();
 
